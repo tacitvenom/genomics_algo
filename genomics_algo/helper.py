27eba653956731471442ee81c19d9a63d99bb3eb
vasu <vasu.sharma314@gmail.com>
2020-11-03 00:36:04 +0100

    Added some more helper functions

--- genomics_algo/helper.py
+++ genomics_algo/helper.py
@@ -1,4 +1,7 @@
-COMPLEMENTARY_BASE = {'A':'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+import math
+
+COMPLEMENTARY_BASE = {"A": "T", "C": "G", "G": "C", "T": "A"}
+
 
 def longest_common_prefix(s1, s2):
     """
@@ -14,10 +17,10 @@ def longest_common_prefix(s1, s2):
         Longest common prefix between s1 and s2
     """
     i = 0
-    while (i < min(len(s1), len(s2))):
+    while i < min(len(s1), len(s2)):
         if s1[i] != s2[i]:
-            break;
-        i+=1
+            break
+        i += 1
     return s1[:i]
 
 
@@ -31,7 +34,7 @@ def reverse_complement(s):
         DNA sequence of the opposite strand in the reverse order
     """
     # TODO: make robust againt garbage values
-    return ''.join([COMPLEMENTARY_BASE[base] for base in s[::-1]])
+    return "".join([COMPLEMENTARY_BASE[base] for base in s[::-1]])
 
 
 def read_genome(filename):
@@ -46,5 +49,62 @@ def read_genome(filename):
         Genome string
     """
     with open(filename) as f:
-        genome = ''.join([line.rstrip() for line in f.readlines() if not line.startswith('>')])
+        genome = "".join(
+            [line.rstrip() for line in f.readlines() if not line.startswith(">")]
+        )
     return genome
+
+
+def read_fastq(filename):
+    """
+    Reads sequences and qualities from a .fastq file
+
+    filename: string
+        relative or absolute path of the .fa file to be read from
+
+    Returns:
+    reads: list
+        List of sequence reads
+    qualities: list
+        List of qualities corresponding to each sequence read
+
+    """
+    reads = []
+    qualities = []
+    with open(filename, "r") as f:
+        while True:
+            f.readline()
+            read = f.readline().rstrip()
+            f.readline()
+            seq_qualities = f.readline().rstrip()
+            if len(read) == 0:
+                break
+            reads.append(read)
+            qualities.append(seq_qualities)
+
+    return reads, qualities
+
+
+def map_phred33_to_error_probability(phred33):
+    """Maps a ASCII phred33 quality character to a quality score in fraction
+    # TODO
+    Args:
+        quality ([type]): [description]
+    """
+    return 10 ** (-phred33 / 10)
+
+
+def map_errorprobability_to_phred33(probability):
+    """
+    # TODO
+    """
+    return -10 * math.log(probability, 10)
+
+
+def map_phred33_ascii_to_qualityscore(phred33_char):
+    """Maps a ASCII phred33 quality character to a quality score
+    # TODO
+    Args:
+        quality ([type]): [description]
+    """
+    return ord(phred33_char) - 33
--- genomics_algo/tests/test_helper.py
+++ genomics_algo/tests/test_helper.py
@@ -1,35 +1,87 @@
 import pytest
 
-from genomics_algo.helper import longest_common_prefix, reverse_complement, read_genome
+from genomics_algo.helper import (
+    longest_common_prefix,
+    reverse_complement,
+    read_genome,
+    read_fastq,
+    map_phred33_to_error_probability,
+    map_errorprobability_to_phred33,
+    map_phred33_ascii_to_qualityscore,
+)
+
+DELTA = 10e-4
+
 
 def test_longest_common_prefix():
-    s1 = 'ACTA'
-    s2 = 'GCCT'
-    assert longest_common_prefix(s1, s2) == ''
-    s1 = 'ACTA'
-    s2 = 'ACT'
-    assert longest_common_prefix(s1, s2) == 'ACT'
-    s1 = 'ACTA'
-    s2 = 'ACT'
-    assert longest_common_prefix(s1, s2) == 'ACT'
-    s1 = 'GATA'
-    s2 = 'GAAT'
-    assert longest_common_prefix(s1, s2) == 'GA'
-    s1 = 'ATGA'
-    s2 = ''
-    assert longest_common_prefix(s1, s2) == ''
-    s1 = ''
-    s2 = 'GCCT'
-    assert longest_common_prefix(s1, s2) == ''
+    s1 = "ACTA"
+    s2 = "GCCT"
+    assert longest_common_prefix(s1, s2) == ""
+    s1 = "ACTA"
+    s2 = "ACT"
+    assert longest_common_prefix(s1, s2) == "ACT"
+    s1 = "ACTA"
+    s2 = "ACT"
+    assert longest_common_prefix(s1, s2) == "ACT"
+    s1 = "GATA"
+    s2 = "GAAT"
+    assert longest_common_prefix(s1, s2) == "GA"
+    s1 = "ATGA"
+    s2 = ""
+    assert longest_common_prefix(s1, s2) == ""
+    s1 = ""
+    s2 = "GCCT"
+    assert longest_common_prefix(s1, s2) == ""
 
 
 def test_reverse_complement():
-    assert reverse_complement('ATGC') == 'GCAT'
-    assert reverse_complement('') == ''
+    assert reverse_complement("ATGC") == "GCAT"
+    assert reverse_complement("") == ""
     with pytest.raises(KeyError):
-        reverse_complement('AGRTTTAG')
+        reverse_complement("AGRTTTAG")
 
 
 def test_read_genome():
-    genome = read_genome('genomics_algo/tests/test_data/lambda_virus.fa')
+    genome = read_genome("genomics_algo/tests/test_data/lambda_virus.fa")
     assert len(genome) == 48502
+    assert genome[:50] == "GGGCGGCGACCTCGCGGGTTTTCGCTATTTATGAAAATTTTCCGGTTTAA"
+
+
+def test_read_fastq():
+    reads, qualities = read_fastq(
+        "genomics_algo/tests/test_data/SRR835775_1.first1000.fastq"
+    )
+    assert len(reads) == len(qualities) == 1000
+    assert (
+        reads[0]
+        == "TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTCACCCTAACCCTAACCCTAACCGTATCCGTCACCCTAACCCTAAC"
+    )
+    assert (
+        qualities[0]
+        == "???B1ADDD8??BB+C?B+:AA883CEE8?C3@DDD3)?D2;DC?8?=BAD=@C@(.6.6=A?=?@##################################"
+    )
+
+
+def test_map_phred33_to_error_probability():
+    phred33_to_error_probability = {
+        10: 0.1,
+        20: 0.01,
+        50: 0.00001,
+    }
+    for score, probability in phred33_to_error_probability.items():
+        assert abs(map_phred33_to_error_probability(score) - probability) < DELTA
+
+
+def test_map_errorprobability_to_phred33():
+    error_probability_to_phred33 = {
+        0.1: 10,
+        0.01: 20,
+        0.00001: 50,
+    }
+    for probability, score in error_probability_to_phred33.items():
+        assert abs(map_errorprobability_to_phred33(probability) - score) < DELTA
+
+
+def test_map_phred33_ascii_to_qualityscore():
+    assert map_phred33_ascii_to_qualityscore("#") == 2
+    assert map_phred33_ascii_to_qualityscore("J") == 41
